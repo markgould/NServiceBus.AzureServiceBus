7b01d84c0da133c6671c69a802299a6be1caf0eb
danielmarbach <daniel.marbach@openplace.net>
2018-11-08 11:36:40 +0100

    Added test to verify that hierarchy is also supported

--- src/Tests/Creation/When_creating_subscription.cs
+++ src/Tests/Creation/When_creating_subscription.cs
@@ -15,22 +15,31 @@
     public class When_creating_subscription
     {
         const string topicPath = "topic";
+        const string hierarchyTopicPath = "hierarchy/tenant1/topic";
         static SubscriptionMetadataInternal metadata = new SubscriptionMetadataInternal { Description = "eventname" };
         const string sqlFilter = "1=1";
 
         [OneTimeSetUp]
         public void TopicSetup()
         {
-            var namespaceManager = new NamespaceManagerAdapterInternal(NamespaceManager.CreateFromConnectionString(AzureServiceBusConnectionString.Value));
-            if (!namespaceManager.TopicExists(topicPath).Result)
+            var manager = new NamespaceManagerAdapterInternal(NamespaceManager.CreateFromConnectionString(AzureServiceBusConnectionString.Value));
+            if (!manager.TopicExists(topicPath).Result)
             {
-                namespaceManager.CreateTopic(new TopicDescription(topicPath)).GetAwaiter().GetResult();
+                manager.CreateTopic(new TopicDescription(topicPath)).GetAwaiter().GetResult();
+            }
+            if (!manager.TopicExists(hierarchyTopicPath).Result)
+            {
+                manager.CreateTopic(new TopicDescription(hierarchyTopicPath)).GetAwaiter().GetResult();
             }
 
-            namespaceManager = new NamespaceManagerAdapterInternal(NamespaceManager.CreateFromConnectionString(AzureServiceBusConnectionString.Fallback));
-            if (!namespaceManager.TopicExists(topicPath).Result)
+            manager = new NamespaceManagerAdapterInternal(NamespaceManager.CreateFromConnectionString(AzureServiceBusConnectionString.Fallback));
+            if (!manager.TopicExists(topicPath).Result)
             {
-                namespaceManager.CreateTopic(new TopicDescription(topicPath)).GetAwaiter().GetResult();
+                manager.CreateTopic(new TopicDescription(topicPath)).GetAwaiter().GetResult();
+            }
+            if (!manager.TopicExists(hierarchyTopicPath).Result)
+            {
+                manager.CreateTopic(new TopicDescription(hierarchyTopicPath)).GetAwaiter().GetResult();
             }
         }
 
@@ -39,9 +48,11 @@
         {
             var namespaceManager = new NamespaceManagerAdapterInternal(NamespaceManager.CreateFromConnectionString(AzureServiceBusConnectionString.Value));
             namespaceManager.DeleteTopic(topicPath).GetAwaiter().GetResult();
+            namespaceManager.DeleteTopic(hierarchyTopicPath).GetAwaiter().GetResult();
 
             namespaceManager = new NamespaceManagerAdapterInternal(NamespaceManager.CreateFromConnectionString(AzureServiceBusConnectionString.Fallback));
             namespaceManager.DeleteTopic(topicPath).GetAwaiter().GetResult();
+            namespaceManager.DeleteTopic(hierarchyTopicPath).GetAwaiter().GetResult();
         }
 
         [Test]
@@ -269,6 +280,29 @@
             Assert.IsNull(foundDescription.ForwardTo);
 
             await namespaceManager.DeleteSubscription(new SubscriptionDescription(topicPath, subscriptionName));
+            await namespaceManager.DeleteQueue(queueToForwardTo.Path);
+        }
+        
+        [Test]
+        public async Task Should_properly_set_ForwardTo_on_the_created_entity_with_hierarchy()
+        {
+            var namespaceManager = new NamespaceManagerAdapterInternal(NamespaceManager.CreateFromConnectionString(AzureServiceBusConnectionString.Value));
+
+            var queueCreator = new AzureServiceBusQueueCreator(new TopologyQueueSettings(), DefaultConfigurationValues.Apply(SettingsHolderFactory.BuildWithSerializer()));
+            var queueToForwardTo = await queueCreator.Create("forwardto", namespaceManager);
+
+            var creator = new AzureServiceBusSubscriptionCreator(new TopologySubscriptionSettings());
+
+            const string subscriptionName = "sub17";
+            await creator.Create(hierarchyTopicPath, subscriptionName, metadata, sqlFilter, namespaceManager, queueToForwardTo.Path);
+            // create again without forward to
+            await creator.Create(hierarchyTopicPath, subscriptionName, metadata, sqlFilter, namespaceManager);
+
+            var foundDescription = await namespaceManager.GetSubscription(hierarchyTopicPath, subscriptionName);
+
+            Assert.IsNull(foundDescription.ForwardTo);
+
+            await namespaceManager.DeleteSubscription(new SubscriptionDescription(hierarchyTopicPath, subscriptionName));
             await namespaceManager.DeleteQueue(queueToForwardTo.Path);
         }
 
